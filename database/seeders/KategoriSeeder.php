7fb6e8220f64a6ea9cb9c3f60727c57fe9dae28a
sheiyyara <sheilandrazarawiba561@gmail.com>
2026-04-19 20:42:04 +0700

    setup seeder for level, kategori, and user

---
+++ database/seeders/KategoriSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KategoriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('m_kategori')->insert([
+            ['kategori_kode' => 'KTG01', 'kategori_nama' => 'Makanan'],
+            ['kategori_kode' => 'KTG02', 'kategori_nama' => 'Minuman'],
+        ]);
+    }
+}
