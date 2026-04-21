dada0bdec992af2a0af7d6a08b294a28fc2418c6
sheiyyara <sheilandrazarawiba561@gmail.com>
2026-04-21 23:30:00 +0700

    fix(penjualan): fix create penjualan details

--- app/Filament/Resources/PenjualanDetails/Pages/CreatePenjualanDetail.php
+++ app/Filament/Resources/PenjualanDetails/Pages/CreatePenjualanDetail.php
@@ -3,9 +3,27 @@
 namespace App\Filament\Resources\PenjualanDetails\Pages;
 
 use App\Filament\Resources\PenjualanDetails\PenjualanDetailResource;
+use App\Models\Stok;
+use Exception;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePenjualanDetail extends CreateRecord
 {
     protected static string $resource = PenjualanDetailResource::class;
+
+    protected function afterCreate(): void
+    {
+        $stok = Stok::where('barang_id', $this->record->barang_id)->first();
+
+        if (!$stok) {
+            throw new Exception('Stok tidak ditemukan');
+        }
+
+        if ($this->record->jumlah > $stok->stok_jumlah) {
+            throw new Exception('Stok tidak cukup');
+        }
+
+        $stok->stok_jumlah -= $this->record->jumlah;
+        $stok->save();
+    }
 }
